0d320309247020fa6fced7340ef0bb777e53a2e9
Xavier Mathieu <xavier2003@hotmail.fr>
2022-03-26 14:40:09 -0400

    Refactor: Program handles the process

--- src/Httpd.CLI/Program.cs
+++ src/Httpd.CLI/Program.cs
@@ -1,4 +1,15 @@
 ﻿using Httpd;
 
 var server = new Server(3000);
-await server.Start();
+await ManageServer();
+
+async Task ManageServer()
+{
+    while (true)
+    {
+        var client = await server.GetRequest();
+
+        var request = new Request();
+        request.HandleRequest(client);
+    }
+}
